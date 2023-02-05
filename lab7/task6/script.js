623dbd73db4d216cfe0df36753d42caa9fcf36ac
soeys <laucefirst@gmail.com>
2023-02-05 10:50:56 -0500

    1

--- lab7/task6/script.js
+++ lab7/task6/script.js
@@ -1,22 +1,23 @@
+let table = document.querySelector('#table-body');
+
 document.querySelector('#add').addEventListener('click', function () {
     let userInput = document.getElementById('additonal-os').value;
-    userInput += " ";
+    if (userInput) {
+        userInput += " ";
+        let tr = document.createElement('tr');
 
-    let userInputRow = document.getElementById('additonal-os').parentElement.parentElement;
+        while (userInput.search(" ") > 0) {
+            let td = document.createElement('td');
 
-    console.log(userInputRow);
-    let a = [];
+            td.appendChild(document.createTextNode(userInput.slice(0, userInput.search(" "))));
+            userInput = userInput.slice(userInput.search(" ") + 1, userInput.length);
 
-    while (userInput.search(" ") > 0) {
-        a.push(userInput.slice(0, userInput.search(" ")));
-        userInput = userInput.slice(userInput.search(" ") + 1, userInput.length);
+            tr.appendChild(td);
+        }
+        table.insertBefore(tr, table.children[document.querySelectorAll('#table-body tr').length - 1]);
     }
-    userInputRow.removeChild(userInputRow.children[0]);
+})
 
-    a.forEach(element => {
-        td = document.createElement('td');
-        td.appendChild(document.createTextNode(element));
-        userInputRow.appendChild(td);
-    });
-
+document.querySelector('#remove').addEventListener('click', function () {
+    table.removeChild(table.children[0]);
 })
