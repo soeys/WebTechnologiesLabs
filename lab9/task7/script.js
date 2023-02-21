d26adcfc85bd8afa7ebd4dc2eb2fe8e7d9f9dfc0
soeys <laucefirst@gmail.com>
2023-02-21 16:51:44 -0500

    lab 9 task 7

--- lab9/task7/script.js
+++ lab9/task7/script.js
@@ -4,19 +4,23 @@ let on = false;
 id = 0;
 let array = [0, 1, 3, 2];
 
-
+function hop() {
+    document.getElementById("box" + array[id % array.length]).style.backgroundColor = "red";
+    for (let index = 0; index < array.length; index++) {
+        if (index == array[id % array.length]) {
+            continue;
+        }
+        document.getElementById("box" + index).style.backgroundColor = "white";
+    }
+    id++;
+}
 
 document.querySelector('#start').addEventListener('click', function () {
-    function hop() {
-        document.getElementById("box" + array[id % array.length]).style.backgroundColor = "red";
-        for (let index = 0; index < array.length; index++) {
-            if (index == id % array.length) {
-                continue;
-            }
-            document.getElementById("box" + index).style.backgroundColor = "white";
-        }
-        id++;
+    on = !on;
+    console.log(on);
+    if (on) {
+        interval = setInterval(hop, 500);
+    } else {
+        clearInterval(interval);
     }
-
-    interval = setInterval(hop, 500)
 })
